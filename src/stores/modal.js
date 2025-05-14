2ea9fdfb735e67e96a02411455a23e22bbf1b848
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-05-14 19:05:51 +1000

    Implement authentication modal functionality; update AppHeader for modal toggle and enhance AuthModal with tab switching

---
+++ src/stores/modal.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useModalStore = defineStore('modal', {
+  state: () => ({
+    isOpen: false,
+  }),
+
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    },
+  },
+})
