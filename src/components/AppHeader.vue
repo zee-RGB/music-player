15e8e579fc3a158587cfc49882d0105c86c49f2d
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-05-27 08:30:42 +1000

    feat: update routing to use named routes for navigation links; enhance readability and maintainability

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,13 +2,18 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</router-link>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Music</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <li>
-            <router-link class="text-white px-2" to="/about">About</router-link>
+            <router-link class="text-white px-2" :to="{ name: `about` }">About</router-link>
           </li>
 
           <!-- Navigation Links -->
@@ -20,7 +25,7 @@
 
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{ name: `manage` }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: HomeView,
   },
   {
+    name: 'about',
     path: '/about',
     component: AboutView,
   },
   {
-    path: '/manage',
+    name: 'manage',
+    path: '/manage-music',
     component: ManageView,
+    // alias: '/manage',
+  },
+
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+
+  {
+    path: '/:catchAll(.*)',
+    redirect: { name: 'home' },
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 })
 
 export default router
