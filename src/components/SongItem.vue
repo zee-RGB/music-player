62f961f0041be2a3c823f53bd3a8b56214efbcd7
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-06-02 16:08:58 +1000

    feat: update SongItem component to use router-link for song navigation; enhance Song view with dynamic song details and comment functionality

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -4,7 +4,11 @@
       class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
     >
       <div>
-        <a href="#" class="font-bold block text-gray-600">{{ song.modified_name }}</a>
+        <rounter-link
+          :to="{ name: 'song', params: { id: song.docID } }"
+          class="font-bold block text-gray-600"
+          >{{ song.modified_name }}</rounter-link
+        >
         <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
       </div>
 
