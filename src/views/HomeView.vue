37287172993c59b2104668b53316bbba94362761
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-06-05 09:36:26 +1000

    feat: update icon directive to support dynamic icon classes and modify HomeView to use new directive for song section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@
   <!-- Main Content -->
   <section class="container mx-auto">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon>
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon.right="headphone - alt">
         <span class="card-title">Songs</span>
         <!-- Icon -->
       </div>
